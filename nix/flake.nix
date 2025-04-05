c739d2e2ebc9d5e1f92e089fdd118a3f990434f6
Warren Layson <warrenlayson@gmail.com>
2025-04-05 12:35:56 +0800

    update nix configuration, modularize a bit
    moved some of MAS apps to homebrew

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
-      url = "github:LnL7/nix-darwin/master";
+      url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager.url = "github:nix-community/home-manager";
@@ -26,155 +26,81 @@
 
   outputs = inputs@{ self, nixpkgs, nix-darwin, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, home-manager,... }:
   let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
-          pkgs.vim
-        ];
-
-      fonts.packages = [
-        pkgs.nerd-fonts.jetbrains-mono
-      ];
-
-      system.defaults = {
-        dock.autohide = true;
-        dock.persistent-apps = [
-            "${pkgs.alacritty}/Applications/Alacritty.app"
-            "/System/Applications/Mail.app"
-            "/Applications/Safari.app"
-        ];
-        finder.FXPreferredViewStyle = "clmv";
-        finder.AppleShowAllFiles = true;
-        NSGlobalDomain.AppleInterfaceStyle = "Dark";
-      };
-
-      homebrew = {
-        enable = true;
-	brews = [
-	  "docker-credential-helper"
-    "git-lfs"
-	];
-    casks = [
-        "firefox"
-        "httpie"
-        "messenger"
-        "malwarebytes"
-        ];
-	onActivation.cleanup = "zap";
-      };
-
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+    inherit (nixpkgs.lib)
+        attrValues
+        singleton
+        optionalAttrs
+        ;
+    nixpkgsConfig = {
+        config = {allowUnfree = true;};
+        overlays = attrValues self.overlays
+            ++ singleton (
+                final: prev: (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+                    # inherit (final.pkgs-x86)
+                })
+            );
     };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#mbpro
-    darwinConfigurations."mbpro" = nix-darwin.lib.darwinSystem {
-      modules = [
-      	configuration
-	home-manager.darwinModules.home-manager
-	{
-	  users.users.warren = {
-	    name = "warren";
-	    home = "/Users/warren";
-	  };
-	  home-manager.users.warren = { pkgs, lib,...}: {
-        nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-         "raycast"
-        ];
-	    home.packages = [
-	      pkgs.stow
-	      pkgs.neovim
-	      pkgs.raycast
-          pkgs.neofetch
-          pkgs.bitwarden-cli
-          pkgs.ollama
-	    ];
 
-	    home.sessionVariables = {
-	      EDITOR = "vim";
-	    };
-        programs = {
-            pyenv = {
-                enable = true;
-                enableZshIntegration = true;
-            };
-            tmux.enable = true;
-            lazygit.enable = true;
-            fd.enable = true;
-            fzf.enable = true;
-            ripgrep.enable = true;
-            alacritty.enable = true;
-            starship.enable = true;
-            direnv = {
-                enable = true;
-                enableZshIntegration = true;
-                nix-direnv.enable = true;
-            };
-            zsh = {
-              enable = true;
-              enableCompletion = true;
-              autosuggestion.enable = true;
-              syntaxHighlighting.enable = true;
-              oh-my-zsh.enable = false;
-              initExtra = ''
-                neofetch
-              '';
+    # Overlays {{{
 
-              shellAliases = {
-                ll = "ls -l";
-                update = "darwin-rebuild switch --flake ~/.dotfiles/nix#mbpro";
-                sshconfig = "$EDITOR ~/.ssh/config";
-                flakeconfig = "$EDITOR ~/.dotfiles/nix/darwin/flake.nix";
-                zshsource = "source ~/.zshrc";
-                colimaw = "colima -p wingaru";
-              };
-              history = {
-                extended = true;
-              };
-
+    overlays = {
+        apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "arch64-darwin") {
+            pkgs-x86 = import nixpkgs {
+                system = "x86_64-darwin";
+                inherit (nixpkgsConfig) config;
             };
         };
-
-	    home.stateVersion = "24.11";
-
-	  };
-	}
-	nix-homebrew.darwinModules.nix-homebrew
-	{
-	  nix-homebrew = {
-	    enable = true;
-
-	    enableRosetta = true;
-	    user = "warren";
-
-	    taps = {
-	      "homebrew/homebrew-core" = homebrew-core;
-	      "homebrew/homebrew-cask" = homebrew-cask;
-	      "homebrew/homebrew-bundle" = homebrew-bundle;
-	    };
-
-	    mutableTaps = false;
-	  };
-	}
-      ];
     };
+
+    # }}}
+
+    # System Configuration {{{
+
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#mbpro
+    darwinConfigurations = {
+        WarrenBookPro = nix-darwin.lib.darwinSystem {
+            specialArgs = { inherit self; };
+            system = "aarch64-darwin";
+            modules = [
+                ./configuration.nix
+                home-manager.darwinModules.home-manager
+                {
+                    nixpkgs = nixpkgsConfig;
+                    users.users.warren.home = "/Users/warren";
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.users.warren = {
+                        imports = [
+                            ./home.nix
+                        ];
+                	    home.stateVersion = "24.11";
+                    };
+                }
+            	nix-homebrew.darwinModules.nix-homebrew
+            	{
+            	  nix-homebrew = {
+            	    enable = true;
+
+            	    enableRosetta = true;
+            	    user = "warren";
+
+            	    taps = {
+            	      "homebrew/homebrew-core" = homebrew-core;
+            	      "homebrew/homebrew-cask" = homebrew-cask;
+            	      "homebrew/homebrew-bundle" = homebrew-bundle;
+            	    };
+
+            	    mutableTaps = false;
+            	  };
+            	}
+            ];
+        };
+    };
+
+    # }}}
+
   };
 }
