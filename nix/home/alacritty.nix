4f7e694c68660363d12f3a9d061bf733a1e3a59c
Warren Angelo H. Layson <warrenlayson@gmail.com>
2025-04-06 17:14:40 +0800

    update alacritty.nix
    set font

--- nix/home/alacritty.nix
+++ nix/home/alacritty.nix
@@ -1,4 +1,11 @@
 { ... }:
 {
     programs.alacritty.enable = true;
+    programs.alacritty.settings = {
+        font = {
+            normal = { family = "JetBrainsMono Nerd Font"; style = "Regular"; };
+            size = 13;
+        };
+
+    };
 }
